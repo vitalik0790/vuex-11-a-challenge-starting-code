f16d2c44b08267632ecebe2df3ba466dabfba6a9
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-05-05 18:56:14 +0300

    finish

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 
 import router from './router.js';
 import App from './App.vue';
+import store from './store/index'
 import BaseBadge from './components/ui/BaseBadge.vue';
 
 const app = createApp(App)
 
+app.use(store);
 app.use(router);
 
 app.component('base-badge', BaseBadge);
---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import { createStore } from 'vuex';
+
+import productsModule from './modules/products/products';
+import cartModule from './modules/cart/cart'
+
+export default createStore({
+    modules: {
+        prods: productsModule,
+        cart: cartModule,
+    },
+    state() {
+        return {
+            isLoggedIn: false,
+        }
+    },
+    mutations: {
+        login(state) {
+            state.isLoggedIn = true;
+        },
+        logout(state) {
+            state.isLoggedIn = false;
+        }
+    },
+    actions: {
+        login(context) {
+            context.commit('login')
+        },
+        logout(context) {
+            context.commit('logout')
+        },
+    },
+    getters: {
+        isAuthenticated(state) {
+            return state.isLoggedIn;
+        },
+    }
+});
